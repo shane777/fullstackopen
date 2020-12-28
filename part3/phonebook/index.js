cb8f6a43308bb35048833dbb434a2479face3a89
Shane Zhang <lcseven7@hotmail.com>
2020-12-28 16:04:02 +0800

    ui build for part3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors')
 
 const app = express();
 app.use(express.json());
-app.use(cors());
 app.use(express.static('build'));
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
